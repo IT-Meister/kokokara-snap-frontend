e1e721c50e5ac3ec7e1db4a920ef46c6d08a4903
Kojiro Tsugaru <kojirotsugaru@gmail.com>
2024-08-24 18:42:46 +0900

    added PostImageUpload view

--- kokokara-snap/src/components/pages/PostPage.tsx
+++ kokokara-snap/src/components/pages/PostPage.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo, useState } from "react";
+import React, { useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
 import {
   Typography,
