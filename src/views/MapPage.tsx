e320d8da30afce2e27d92b2900f8a5f52a9bd172
mizuki-sleeper <roko.k@hotmail.co.jp>
2024-08-25 17:49:11 +0900

    Add MapPage component
    Introduce MapPage component with full-page map display. Uses MapComponent to render the map, ensuring it's flexibly styled for full viewport height and width.

---
+++ src/views/MapPage.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import React from "react";
+
+import MapComponent from "@/components/Map/MapComponent";
+
+export default function MapPage() {
+  return (
+    <div
+      style={{
+        position: "relative",
+        display: "flex",
+        height: "100vh",
+        width: "100%",
+      }}
+    >
+      {/* Map Component */}
+      <MapComponent />
+    </div>
+  );
+};
