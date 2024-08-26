e4c58bf6df597f7ad368d662af259d1043b41a64
Kojiro Tsugaru <kojirotsugaru@gmail.com>
2024-08-26 12:13:07 +0900

    made userinputs a form & added input validation

--- src/views/PostDetails.tsx
+++ src/views/PostDetails.tsx
@@ -17,19 +17,18 @@ import {
   SelectChangeEvent,
 } from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import { useDropzone } from "react-dropzone";
+import { useRouter } from "next/navigation";
 
 import Header from "@/components/Header";
 
 export default function PostDetails() {
-  const [board, setBoard] = useState("");
-  const [showMoreOptions, setShowMoreOptions] = useState(false);
-
   // for user inputs
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [link, setLink] = useState("");
   const [tags, setTags] = useState("");
+  const [board, setBoard] = useState("");
+  const [showMoreOptions, setShowMoreOptions] = useState(false);
 
   const handleBoardChange = (event: SelectChangeEvent) => {
     setBoard(event.target.value as string);
@@ -39,8 +38,10 @@ export default function PostDetails() {
     setShowMoreOptions(!showMoreOptions);
   };
 
+  const router = useRouter();
   const handlePostClick = () => {
     // Publish button click action
+    router.push("/post/details");
   };
 
   return (
@@ -77,61 +78,73 @@ export default function PostDetails() {
             </Paper>
           </Grid>
           <Grid item xs={12} sm={8}>
-            <TextField
-              label="タイトル"
-              placeholder="タイトルを追加する"
-              variant="outlined"
-              fullWidth
-              sx={{ mb: 2 }}
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-            />
-            <TextField
-              label="説明文"
-              placeholder="詳しい説明文を追加する"
-              variant="outlined"
-              fullWidth
-              multiline
-              rows={4}
-              sx={{ mb: 2 }}
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-            />
-            <TextField
-              label="リンク"
-              placeholder="リンクを追加する"
-              variant="outlined"
-              fullWidth
-              sx={{ mb: 2 }}
-              value={link}
-              onChange={(e) => setLink(e.target.value)}
-            />
-            <FormControl fullWidth sx={{ mb: 2 }}>
-              <Select
-                value={board}
-                onChange={handleBoardChange}
-                displayEmpty
-                defaultValue=""
-              >
-                <MenuItem value="">
-                  <em>ボードを選択する</em>
-                </MenuItem>
-                <MenuItem value={1}>ボードA</MenuItem>
-                <MenuItem value={2}>ボードB</MenuItem>
-              </Select>
-            </FormControl>
-            <TextField
-              label="タグを検索する"
-              placeholder="タグを検索する"
-              variant="outlined"
-              fullWidth
-              sx={{ mb: 2 }}
-              value={tags}
-              onChange={(e) => setTags(e.target.value)}
-            />
+            <Box
+              component="form"
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <TextField
+                label="タイトル"
+                placeholder="タイトルを追加する"
+                variant="outlined"
+                fullWidth
+                sx={{ mb: 2 }}
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                required={true}
+              />
+              <TextField
+                label="説明文"
+                placeholder="詳しい説明文を追加する"
+                variant="outlined"
+                fullWidth
+                multiline
+                rows={4}
+                sx={{ mb: 2 }}
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                required={true}
+              />
+              <TextField
+                label="リンク"
+                placeholder="リンクを追加する"
+                variant="outlined"
+                fullWidth
+                sx={{ mb: 2 }}
+                value={link}
+                onChange={(e) => setLink(e.target.value)}
+                required={true}
+              />
+              <FormControl fullWidth sx={{ mb: 2 }}>
+                <Select
+                  value={board}
+                  onChange={handleBoardChange}
+                  displayEmpty
+                  defaultValue=""
+                  required={true}
+                >
+                  <MenuItem value="">
+                    <em>ボードを選択する</em>
+                  </MenuItem>
+                  <MenuItem value={1}>ボードA</MenuItem>
+                  <MenuItem value={2}>ボードB</MenuItem>
+                </Select>
+              </FormControl>
+              <TextField
+                label="タグを追加する"
+                placeholder="タグを追加する"
+                variant="outlined"
+                fullWidth
+                sx={{ mb: 2 }}
+                value={tags}
+                onChange={(e) => setTags(e.target.value)}
+              />
 
-            {/* Additional Options */}
-            <Box sx={{ display: "flex" }}>
+              {/* Additional Options */}
               <Button
                 onClick={toggleMoreOptions}
                 endIcon={<ExpandMoreIcon />}
@@ -139,38 +152,31 @@ export default function PostDetails() {
               >
                 その他のオプション
               </Button>
+              <Collapse in={showMoreOptions}>
+                {/* Additional options content goes here */}
+                <Typography variant="body2" sx={{ mt: 2 }}>
+                  Additional options content...
+                </Typography>
+              </Collapse>
+
+              {/* Save/Publish Button */}
+              <Button
+                variant="contained"
+                type="submit"
+                sx={{
+                  backgroundColor: "#e60023",
+                  color: "#fff",
+                  padding: "8px 24px",
+                  fontSize: "16px",
+                  fontWeight: "bold",
+                }}
+                onClick={handlePostClick}
+              >
+                公開する
+              </Button>
             </Box>
-            <Collapse in={showMoreOptions}>
-              {/* Additional options content goes here */}
-              <Typography variant="body2" sx={{ mt: 2 }}>
-                Additional options content...
-              </Typography>
-            </Collapse>
           </Grid>
         </Grid>
-        {/* Save/Publish Button */}
-        <Box
-          className="Publish Button"
-          sx={{
-            display: "flex",
-            justifyContent: "flex-end",
-            mb: 4,
-          }}
-        >
-          <Button
-            variant="contained"
-            sx={{
-              backgroundColor: "#e60023",
-              color: "#fff",
-              padding: "8px 24px",
-              fontSize: "16px",
-              fontWeight: "bold",
-            }}
-            onClick={handlePostClick}
-          >
-            公開する
-          </Button>
-        </Box>
       </Container>
     </Box>
   );
