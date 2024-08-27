484b155106f0a011e001c8a1cb3eb693a37b224e
mizuki-sleeper <roko.k@hotmail.co.jp>
2024-08-27 20:57:27 +0900

    Add Layout component
    Introduce a new Layout component to encapsulate the structure of pages. Includes a Header component and a section for child components, ensuring consistent layout across the application.

---
+++ src/views/Layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Header from "@/components/Header";
+
+export default function Layout({children}: Readonly<{ children: React.ReactNode; }>) {
+  return (
+    <>
+      <Header/>
+      {children}
+    </>
+  )
+}
